7dc147fa470b6315779b117a8c2d9fb157547731
Enternikot <47455848+Enternikot@users.noreply.github.com>
2025-04-16 21:16:08 +0200

    Index Hero and List Content

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,11 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'NuxtAnime',
+    description: 'Build beautiful Animation with Nuxt & Anime.js.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      github: 'Enternikot/NuxtAnime',
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
---
+++ components/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>
+        <strong style="color: #00DC82">
+            <span>Nuxt</span>
+            <span style="color: #fff; font-weight: 400; font-style: italic;">Anime</span>
+            <span style="color: #ff4b4b;">.</span>
+        </strong>
+    </h1>
+</template>
